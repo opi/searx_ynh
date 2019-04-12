6cc38c8dfc9a31ed9585fcf688f6749e0c0aaaaf
Pierre Bourré <pierre.moltess@gmail.com>
2019-04-13 00:38:47 +0200

    potential solution

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 #=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="git build-essential libxslt-dev python-dev python-virtualenv python-cffi virtualenv python-babel zlib1g-dev libffi-dev libssl-dev python-lxml uwsgi uwsgi-plugin-python"
+
+#=================================================
 # FUTURE OFFICIAL HELPERS
 #=================================================
 
