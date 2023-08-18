4c596efcba00c06f527f3d8ff2238c3a226549db
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-08-18 11:43:21 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,9 +4,6 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-#REMOVEME? pkg_dependencies="git build-essential libxslt-dev python3-dev python3-venv python3-cffi python3-babel zlib1g-dev libffi-dev libssl-dev python3-lxml uwsgi uwsgi-plugin-python3 shellcheck"
-
 #=================================================
 # UWSGI HELPERS
 #=================================================
